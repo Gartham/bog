0751ef3194a255bbd92d06169cd064b94cac4178
Gartham <gartham@dusttoash.org>
2022-07-21 23:24:13 -0500

    Update Bog.java

--- src/com/gartham/discord/bots/furry/Bog.java
+++ src/com/gartham/discord/bots/furry/Bog.java
@@ -1,19 +1,28 @@
 package com.gartham.discord.bots.furry;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import javax.security.auth.login.LoginException;
 
+import org.alixia.javalibrary.json.JSONObject;
+import org.alixia.javalibrary.json.JSONParser;
 import org.alixia.javalibrary.parsers.cli.CLIParams;
+import org.alixia.javalibrary.streams.CharacterStream;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
-import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Bog {
@@ -23,16 +32,52 @@ public class Bog {
 		bog.start();
 	}
 
+	private final Map<String, UserData> data = new HashMap<>();
+
 	/**
 	 * Loads a user's {@link UserData} object given their ID. Uses the directory
-	 * {@link BotConfiguration#getUserDataPath()} in {@link #config}.
+	 * {@link BotConfiguration#getUserDataPath()} in {@link #config}. A new
+	 * {@link UserData} is only created if one is not found in {@link #data}.
 	 * 
-	 * @param userid
-	 * @return
+	 * @param userid The id of the user to load the data object of.
+	 * @return A {@link UserData} object that can be edited and flushed. Only one of
+	 *         these should really be made during the program's life-cycle. The
+	 *         program does not yet support reading from the file (nor two separate
+	 *         {@link UserData}s representing the same user, since they would not be
+	 *         able to synchronize edits made on each other).
 	 */
-	public UserData load(String userid) {
+	public synchronized UserData getUserData(String userid) {
+		if (data.containsKey(userid))
+			return data.get(userid);
 		var file = new File(config.getUserDataPath(), userid);
-		return new UserData(file);
+
+		JSONObject obj = new JSONObject();
+		if (file.isFile())
+			try (InputStreamReader isr = new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8)) {
+				JSONParser parser = new JSONParser();
+				obj = (JSONObject) parser.parse(CharacterStream.from(isr));
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassCastException e) {
+				System.err.println("Invalid data inside user data file: " + file + ". File ignored. (USER " + userid
+						+ " HAS NOT HAD DATA LOADED.)");
+			}
+		var userData = new UserData() {
+			/**
+			 * SUID
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void flush() throws IOException {
+				try (PrintWriter pw = new PrintWriter(file)) {
+					pw.print(toString());
+				}
+			}
+		};
+		userData.putAll(obj);
+		data.put(userid, userData);
+		return userData;
 	}
 
 	private final BotConfiguration config;
@@ -57,7 +102,7 @@ public class Bog {
 		jda.addEventListener((EventListener) event -> {
 			if (event instanceof SlashCommandInteractionEvent) {
 				var e = (SlashCommandInteractionEvent) event;
-
+				var ud = getUserData(e.getUser().getId());
 			}
 		});
 	}
