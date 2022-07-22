12a5c05fbe0ec14c95f8ba69daaf5b0be1043f8d
Gartham <gartham@dusttoash.org>
2022-07-21 22:49:01 -0500

    Add UserData

--- src/com/gartham/discord/bots/furry/BotConfiguration.java
+++ src/com/gartham/discord/bots/furry/BotConfiguration.java
@@ -1,5 +1,7 @@
 package com.gartham.discord.bots.furry;
 
+import java.io.File;
+
 import org.alixia.javalibrary.parsers.cli.CLIParams;
 
 public class BotConfiguration {
@@ -14,12 +16,18 @@ public class BotConfiguration {
 
 	public BotConfiguration(CLIParams cmdLineArguments) {
 		token = cmdLineArguments.readString("", "--token");
+		userDataPath = new File(cmdLineArguments.readString("", "--user-data-path", "-udp"));
 	}
 
 	private final String token;
+	private final File userDataPath;
 
 	public String getToken() {
 		return token;
+	}
+
+	public File getUserDataPath() {
+		return userDataPath;
 	}
 
 }
---
+++ src/com/gartham/discord/bots/furry/UserData.java
@@ -0,0 +1,21 @@
+package com.gartham.discord.bots.furry;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+import org.alixia.javalibrary.json.JSONObject;
+
+public class UserData extends DataObject implements Toilet {
+
+	/**
+	 * SUID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void flush() throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
