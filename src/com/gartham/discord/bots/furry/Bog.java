357da0703be2cc86784c1569a08116d46af5c808
Gartham <gartham@dusttoash.org>
2022-07-21 22:48:48 -0500

    Refactor BotLauncher->Bog

--- src/com/gartham/discord/bots/furry/Bog.java
+++ src/com/gartham/discord/bots/furry/Bog.java
@@ -1,5 +1,8 @@
 package com.gartham.discord.bots.furry;
 
+import java.io.File;
+import java.util.Random;
+
 import javax.security.auth.login.LoginException;
 
 import org.alixia.javalibrary.parsers.cli.CLIParams;
@@ -13,16 +16,40 @@ import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
-public class BotLauncher {
+public class Bog {
 
 	public static void main(String[] args) throws LoginException, InterruptedException {
-		BotConfiguration config = new BotConfiguration(new CLIParams(args));
+		Bog bog = new Bog(args);
+		bog.start();
+	}
 
+	/**
+	 * Loads a user's {@link UserData} object given their ID. Uses the directory
+	 * {@link BotConfiguration#getUserDataPath()} in {@link #config}.
+	 * 
+	 * @param userid
+	 * @return
+	 */
+	public UserData load(String userid) {
+		var file = new File(config.getUserDataPath(), userid);
+		return new UserData(file);
+	}
+
+	private final BotConfiguration config;
+	private final Random random = new Random();
+
+	public Bog(BotConfiguration config) {
+		this.config = config;
+	}
+
+	public Bog(String... args) {
+		this(new BotConfiguration(new CLIParams(args)));
+	}
+
+	public void start() throws LoginException, InterruptedException {
 		JDA jda = JDABuilder.createDefault(config.getToken(), GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS))
 				.build();
-
 		jda.awaitReady();
-
 		jda.upsertCommand(
 				Commands.slash("beg", "Ask me for cash. I dare you. (Something random happens when you ask.)"))
 				.complete();
@@ -33,6 +60,10 @@ public class BotLauncher {
 
 			}
 		});
+	}
+
+	public BotConfiguration getConfig() {
+		return config;
 	}
 
 }
