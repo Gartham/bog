d5ecc19a7c9168d1e030aac770130ddfe6796729
Gartham <gartham@dusttoash.org>
2022-07-21 23:24:09 -0500

    Update UserData.java

--- src/com/gartham/discord/bots/furry/UserData.java
+++ src/com/gartham/discord/bots/furry/UserData.java
@@ -1,21 +1,18 @@
 package com.gartham.discord.bots.furry;
 
 import java.io.IOException;
-import java.math.BigInteger;
 
-import org.alixia.javalibrary.json.JSONObject;
-
-public class UserData extends DataObject implements Toilet {
+public abstract class UserData extends DataObject implements Toilet {
 
 	/**
 	 * SUID
 	 */
 	private static final long serialVersionUID = 1L;
 
-	@Override
-	public void flush() throws IOException {
-		// TODO Auto-generated method stub
+	private final BigIntegerEntry balance = new BigIntegerEntry("balance");
 
+	public BigIntegerEntry getBalance() {
+		return balance;
 	}
 
 }
