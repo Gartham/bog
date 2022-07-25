6286e47341f5dec62fc279e0fc5202e3ebf1254b
Gartham <gartham@dusttoash.org>
2022-07-25 15:34:29 -0500

    Update AbstractSlashCommand.java

--- src/com/gartham/discord/bots/furry/commands/AbstractSlashCommand.java
+++ src/com/gartham/discord/bots/furry/commands/AbstractSlashCommand.java
@@ -2,16 +2,15 @@ package com.gartham.discord.bots.furry.commands;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
-public abstract class AbstractSlashCommand implements SlashCommand {
-	private final String name;
+public abstract class AbstractSlashCommand extends SlashCommand {
 
-	public AbstractSlashCommand(String name) {
-		this.name = name;
+	public AbstractSlashCommand(String name, String description) {
+		super(name, description);
 	}
 
 	@Override
 	public final void onEvent(SlashCommandInteractionEvent e) {
-		if (e.getName().equals(name))
+		if (e.getName().equals(getName()))
 			handle(e);
 	}
 
