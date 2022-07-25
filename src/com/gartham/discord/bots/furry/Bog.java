388a446999d7d85e91eb135b5cf4943835f4a50f
Gartham <gartham@dusttoash.org>
2022-07-25 09:12:41 -0500

    Update Bog.java

--- src/com/gartham/discord/bots/furry/Bog.java
+++ src/com/gartham/discord/bots/furry/Bog.java
@@ -100,43 +100,48 @@ public class Bog {
 		jda.upsertCommand(
 				Commands.slash("beg", "Ask me for cash. I dare you. (Something random happens when you ask.)"))
 				.complete();
+		jda.upsertCommand(Commands.slash("word", "Pick the right definition and you'll be rewarded."));
 
-		Map<String, Instant> lastUse = new HashMap<>();
+		Map<String, Instant> lastBegUse = new HashMap<>();
 
 		jda.addEventListener((EventListener) event -> {
 			if (event instanceof SlashCommandInteractionEvent) {
 				var e = (SlashCommandInteractionEvent) event;
+				if (e.getName().equals("beg")) {
 
-				if (lastUse.containsKey(e.getUser().getId())) {
-					long sec = Duration.between(lastUse.get(e.getUser().getId()), Instant.now()).abs().toSeconds();
-					if (sec < 45) {
-						e.reply("You're running that command too fast! You need to wait " + (45 - sec)
-								+ " seconds before reusing it.").complete();
-						return;
+					if (lastBegUse.containsKey(e.getUser().getId())) {
+						long sec = Duration.between(lastBegUse.get(e.getUser().getId()), Instant.now()).abs().toSeconds();
+						if (sec < 45) {
+							e.reply("You're running that command too fast! You need to wait " + (45 - sec)
+									+ " seconds before reusing it.").complete();
+							return;
+						}
 					}
+					lastBegUse.put(e.getUser().getId(), Instant.now());
+
+					var ud = getUserData(e.getUser().getId());
+					int amount = random.nextInt(35) + 25;
+					ud.getBalance().increase(amount);
+					try {
+						ud.flush();
+					} catch (IOException e1) {
+						System.err.println("Failed to flush " + e.getUser().getId());
+					}
+
+					var x = switch (random.nextInt(6)) {
+					case 0 -> "while parachuting.";
+					case 1 -> "at a murder scene.";
+					case 2 -> "behind your ear.";
+					case 3 -> "in a desk drawer.";
+					case 4 -> "under a tree.";
+					default -> "in a locker.";
+					};
+
+					e.reply("You found " + Utilities.format(amount) + ' ' + x + " You now have "
+							+ Utilities.format(ud.getBalance().get()) + '.').complete();
+				} else if (e.getName().equals("word")) {
+					
 				}
-				lastUse.put(e.getUser().getId(), Instant.now());
-
-				var ud = getUserData(e.getUser().getId());
-				int amount = random.nextInt(35) + 25;
-				ud.getBalance().increase(amount);
-				try {
-					ud.flush();
-				} catch (IOException e1) {
-					System.err.println("Failed to flush " + e.getUser().getId());
-				}
-
-				var x = switch (random.nextInt(6)) {
-				case 0 -> "while parachuting.";
-				case 1 -> "at a murder scene.";
-				case 2 -> "behind your ear.";
-				case 3 -> "in a desk drawer.";
-				case 4 -> "under a tree.";
-				default -> "in a locker.";
-				};
-
-				e.reply("You found " + Utilities.format(amount) + ' ' + x + " You now have "
-						+ Utilities.format(ud.getBalance().get()) + '.').complete();
 
 			}
 		});
