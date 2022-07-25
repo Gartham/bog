18446dbb5d700e42f0935d4a6c8e083f4ef11940
Gartham <gartham@dusttoash.org>
2022-07-25 10:42:24 -0500

    Update Bog.java

--- src/com/gartham/discord/bots/furry/Bog.java
+++ src/com/gartham/discord/bots/furry/Bog.java
@@ -8,23 +8,38 @@ import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.security.auth.login.LoginException;
 
+import org.alixia.javalibrary.JavaTools;
 import org.alixia.javalibrary.json.JSONObject;
 import org.alixia.javalibrary.json.JSONParser;
 import org.alixia.javalibrary.parsers.cli.CLIParams;
 import org.alixia.javalibrary.streams.CharacterStream;
 
+import com.gartham.utilities.bog.dictionary.parser.DictionaryEntryParser;
+import com.gartham.utilities.bog.dictionary.parser.DictionaryEntryParser.Entry;
+import com.gartham.utilities.bog.dictionary.parser.DictionaryEntrySplitter;
+
 import gartham.c10ver.utils.Utilities;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Bog {
@@ -94,15 +109,30 @@ public class Bog {
 	}
 
 	public void start() throws LoginException, InterruptedException {
+		File dir = new File("rsc/");
+		File[] files = dir.listFiles();
+		Map<String, List<Entry>> entriesByType = new HashMap<>();
+		for (File f : files)
+			try {
+				DictionaryEntryParser dep = new DictionaryEntryParser(new DictionaryEntrySplitter(
+						CharacterStream.from(new InputStreamReader(new FileInputStream(f), StandardCharsets.UTF_8))));
+				while (dep.peek() != null)
+					JavaTools.putIntoListMap(entriesByType, dep.peek().getPos(), dep.next(), ArrayList::new);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
 		JDA jda = JDABuilder.createDefault(config.getToken(), GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS))
 				.build();
 		jda.awaitReady();
-		jda.upsertCommand(
-				Commands.slash("beg", "Ask me for cash. I dare you. (Something random happens when you ask.)"))
+		jda.updateCommands()
+				.addCommands(
+						Commands.slash("beg", "Ask me for cash. I dare you. (Something random happens when you ask.)"),
+						Commands.slash("word", "Pick the right definition and you'll be rewarded."))
 				.complete();
-		jda.upsertCommand(Commands.slash("word", "Pick the right definition and you'll be rewarded."));
 
 		Map<String, Instant> lastBegUse = new HashMap<>();
+		Timer timer = new Timer();
 
 		jda.addEventListener((EventListener) event -> {
 			if (event instanceof SlashCommandInteractionEvent) {
@@ -110,7 +140,8 @@ public class Bog {
 				if (e.getName().equals("beg")) {
 
 					if (lastBegUse.containsKey(e.getUser().getId())) {
-						long sec = Duration.between(lastBegUse.get(e.getUser().getId()), Instant.now()).abs().toSeconds();
+						long sec = Duration.between(lastBegUse.get(e.getUser().getId()), Instant.now()).abs()
+								.toSeconds();
 						if (sec < 45) {
 							e.reply("You're running that command too fast! You need to wait " + (45 - sec)
 									+ " seconds before reusing it.").complete();
@@ -140,11 +171,98 @@ public class Bog {
 					e.reply("You found " + Utilities.format(amount) + ' ' + x + " You now have "
 							+ Utilities.format(ud.getBalance().get()) + '.').complete();
 				} else if (e.getName().equals("word")) {
-					
+					Entry[] entries = new Entry[3];
+					for (int i = 0; i < entries.length; i++) {
+						int type = random.nextInt(4);
+						var t = entriesByType.get(switch (type) {
+						case 0 -> "n.";
+						case 1 -> "adv.";
+						case 2 -> "a.";
+						default -> "v. t.";
+						});
+						int ind = random.nextInt(t.size());
+						while (entries[i] == t.get(ind))
+							ind++;
+						entries[i] = t.get(ind);
+					}
+
+					int ind = random.nextInt(entries.length);
+					var entry = entries[ind];
+
+					var id = e.getUser().getId();
+					var time = Instant.now();
+
+					class CEL implements EventListener {
+						private boolean removed;
+						private InteractionHook repl;
+						private final TimerTask task = new TimerTask() {
+
+							@Override
+							public void run() {
+								try {
+									remove();
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							}
+						};
+
+						@Override
+						public void onEvent(GenericEvent event) {
+							if (Duration.between(Instant.now(), time).toSeconds() >= 45)
+								remove();
+							else if (event instanceof ButtonInteractionEvent) {
+								var e = (ButtonInteractionEvent) event;
+								if (e.getUser().getId().equals(id)) {
+									if (e.getComponentId().equals(String.valueOf(ind + 1))) {
+										var amt = random.nextInt(200) + 60;
+										e.reply("You picked right and earned " + Utilities.format(amt) + '.')
+												.complete();
+										var ud = getUserData(id);
+										ud.getBalance().increase(amt);
+										try {
+											ud.flush();
+										} catch (IOException ex) {
+											System.err.println("Failed to flush " + e.getUser().getId());
+										}
+									} else
+										e.reply("Wrong. The definition of **" + entry.getWord() + "** is: "
+												+ entry.getDefinition()).complete();
+									remove();
+								}
+							}
+						}
+
+						private void remove() {
+							removed = true;
+							repl.editOriginalComponents(ActionRow.of(
+									Button.primary("1", Emoji.fromUnicode("U+0031U+FE0FU+20E3")).asDisabled(),
+									Button.primary("2", Emoji.fromUnicode("U+0032U+FE0FU+20E3")).asDisabled(),
+									Button.primary("3", Emoji.fromUnicode("U+0033 U+FE0F U+20E3")).asDisabled()))
+									.complete();
+							jda.removeEventListener(this);
+
+						}
+					}
+
+					CEL cel = new CEL();
+					jda.addEventListener(cel);
+
+					cel.repl = e
+							.reply("Pick the right definition for this word: **" + entry.getWord()
+									+ "**. You have `45` seconds. \n\n:one: " + entries[0].getDefinition() + "\n:two: "
+									+ entries[1].getDefinition() + "\n:three: " + entries[2].getDefinition())
+							.addActionRow(Button.primary("1", Emoji.fromUnicode("U+0031U+FE0FU+20E3")),
+									Button.primary("2", Emoji.fromUnicode("U+0032U+FE0FU+20E3")),
+									Button.primary("3", Emoji.fromUnicode("U+0033 U+FE0F U+20E3")))
+							.complete();
+					timer.schedule(cel.task, 45000);
+
 				}
 
 			}
 		});
+
 	}
 
 	public BotConfiguration getConfig() {
