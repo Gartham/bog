aac10704192916a5c33d94e5f91cfca98fb7c661
Gartham <gartham@dusttoash.org>
2022-07-25 11:07:40 -0500

    Create AbstractSlashCommand.java

---
+++ src/com/gartham/discord/bots/furry/commands/AbstractSlashCommand.java
@@ -0,0 +1,20 @@
+package com.gartham.discord.bots.furry.commands;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public abstract class AbstractSlashCommand implements SlashCommand {
+	private final String name;
+
+	private AbstractSlashCommand(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public final void onEvent(SlashCommandInteractionEvent e) {
+		if (e.getName().equals(name))
+			handle(e);
+	}
+
+	protected abstract void handle(SlashCommandInteractionEvent e);
+
+}
