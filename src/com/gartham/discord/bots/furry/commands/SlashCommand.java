0eda2f2a5ace4311d537629e7a79c3ce5ca3344f
Gartham <gartham@dusttoash.org>
2022-07-25 15:34:04 -0500

    Update SlashCommand.java

--- src/com/gartham/discord/bots/furry/commands/SlashCommand.java
+++ src/com/gartham/discord/bots/furry/commands/SlashCommand.java
@@ -3,13 +3,37 @@ package com.gartham.discord.bots.furry.commands;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
+import net.dv8tion.jda.api.interactions.commands.Command;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 
-public interface SlashCommand extends EventListener {
+public abstract class SlashCommand implements EventListener {
+
+	private final String name, description;
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public SlashCommand(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
 	@Override
-	default void onEvent(GenericEvent event) {
+	public void onEvent(GenericEvent event) {
 		if (event instanceof SlashCommandInteractionEvent)
 			onEvent((SlashCommandInteractionEvent) event);
 	}
 
-	void onEvent(SlashCommandInteractionEvent e);
+	public abstract void onEvent(SlashCommandInteractionEvent e);
+
+	public SlashCommandData makeCommand() {
+		return Commands.slash(name, description);
+	}
+	
 }
